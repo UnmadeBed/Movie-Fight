13dc402b30aa7be87df7aae8ad54cb3e0b3d4518
unmadebed <kevin.archer@live.com>
2021-07-24 23:10:23 +0100

    set-up .gitignore and config file

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hi there!');
+const mykey = config.MY_KEY;
+const fetchData = async () => {
+  const response = await axios.get('http://www.omdbapi.com', {
+    params: {
+      apikey: mykey,
+      s: 'avengers'
+    }
+  });
+
+  console.log(response.data);
+}
+
+fetchData();
