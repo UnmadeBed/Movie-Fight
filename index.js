1824db33f0f4ae99e6776c6dac76afd390b01b0e
unmadebed <kevin.archer@live.com>
2021-07-26 12:37:34 +0100

    move debouncer into seperate file

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// fetchData function which makes network requests to API
 const mykey = config.MY_KEY;
 const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com', {
@@ -11,25 +12,8 @@ const fetchData = async (searchTerm) => {
 };
 
 const input = document.querySelector('input');
-// This is called 'debouncing an iput'. This means we wait for some time to pass
-// the last event to actually do something.
-
-// Debounce function
-const debounce = (func, delay = 1000) => {
-  let timeOutId;
-  return (...args) => {
-    if (timeOutId) {
-      clearTimeout(timeOutId);
-    }
-    timeOutId = setTimeout(() => {
-      func.apply(null, args);
-    }, delay)
-  };
-};
- 
 
 const onInput = event => {
   fetchData(event.target.value)
 };
-
 input.addEventListener('input', debounce(onInput, 500));
