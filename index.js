eb4311847c85320ba6b8062e38f597eaf7a6ace5
unmadebed <kevin.archer@live.com>
2021-07-29 01:41:06 +0100

    add logic to control for movie with no poster img

--- index.js
+++ index.js
@@ -33,13 +33,15 @@ const resultsWrapper = document.querySelector('.results');
 const onInput = async event => {
   const movies = await fetchData(event.target.value)
 
+  resultsWrapper.innerHTML = '';
   dropdown.classList.add('is-active');
   for (let movie of movies) {
     const option = document.createElement('a');
+    const imageSrc = movie.Poster === 'N/A' ? '' : movie.Poster;
 
     option.classList.add('dropdown-item')
     option.innerHTML = `
-      <img src="${movie.Poster}"/>
+      <img src="${imageSrc}"/>
       ${movie.Title}
     `;
 
--- utils.js
+++ utils.js
@@ -1,3 +1,6 @@
+// This function places a time delay on network requests. It will only make a network request once a
+// period of one second of no input has passed.
+
 const debounce = (func, delay = 1000) => {
   let timeOutId;
   return (...args) => {
